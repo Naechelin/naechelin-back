515e4029caf97bdbb69cea3eb0dec2639762964c
jeonka1001 <52381846+jeonka1001@users.noreply.github.com>
2020-08-15 15:14:04 +0900

    LikeyVO, MemoVO, ReviewVO 추가 #jeonka

---
+++ Naechelin/src/main/java/cf/naechelin/vo/LikeyVO.java
@@ -0,0 +1,54 @@
+package cf.naechelin.vo;
+
+import java.util.Objects;
+
+public class LikeyVO
+{
+    private int storeId;
+    private int memberId;
+
+    public LikeyVO() { }
+
+    public LikeyVO(int storeId, int memberId)
+    {
+        this.storeId = storeId;
+        this.memberId = memberId;
+    }
+
+    public void setStoreId(int storeId)
+    {
+        this.storeId = storeId;
+    }
+
+    public void setMemberId(int memberId)
+    {
+        this.memberId = memberId;
+    }
+
+    public int getStoreId()
+    {
+        return storeId;
+    }
+
+    public int getMemberId()
+    {
+        return memberId;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LikeyVO likeyVO = (LikeyVO) o;
+        return storeId == likeyVO.storeId &&
+                memberId == likeyVO.memberId;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(storeId, memberId);
+    }
+
+}
