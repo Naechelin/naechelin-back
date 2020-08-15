b5ceca37698757659bb75a4c308845c96d76e326
WOOSERK <syeokim@naver.com>
2020-08-15 20:35:02 +0900

    test..zz

---
+++ Naechelin/src/main/java/cf/naechelin/controller/CouponController.java
@@ -0,0 +1,37 @@
+package cf.naechelin.controller;
+
+import cf.naechelin.service.CouponService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CouponController
+{
+    private CouponService service;
+
+    public CouponController() {}
+
+    public String insert()
+    {
+        return "insert";
+    }
+
+    public String delete()
+    {
+        return "delete";
+    }
+
+    public String view()
+    {
+        return "view";
+    }
+
+    public String list()
+    {
+        return "list";
+    }
+
+    public String use()
+    {
+        return "use";
+    }
+}
---
+++ Naechelin/src/main/java/cf/naechelin/service/LineService.java
@@ -0,0 +1,6 @@
+package cf.naechelin.service;
+
+public interface LineService
+{
+    void doService();
+}
