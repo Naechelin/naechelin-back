38b8c8d4538024675415a3924798103ccd9113af
jeonka1001 <ruddks2009@naver.com>
2020-08-15 21:54:12 +0900

    viewList->list Service add

--- Naechelin/src/main/java/cf/naechelin/dao/LikeyDAOImpl.java
+++ Naechelin/src/main/java/cf/naechelin/dao/LikeyDAOImpl.java
@@ -34,7 +34,7 @@ public class LikeyDAOImpl implements LikeyDAO
     @Override
     public List<LikeyVO> LikeStoreList(QueryVO query) throws LikeyException
     {
-        return likeyMapper.viewLikeStoreList(query);
+        return likeyMapper.LikeStoreList(query);
     }
 
     @Override
