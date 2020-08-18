525d000bf7cfe13e60545020e8bfed2cae03c5b5
jeonka1001 <ruddks2009@naver.com>
2020-08-18 15:56:04 +0900

    ReviewDeleteService

--- Naechelin/src/main/java/cf/naechelin/service/review/ReviewUpdateServiceImpl.java
+++ Naechelin/src/main/java/cf/naechelin/service/review/ReviewUpdateServiceImpl.java
@@ -2,9 +2,13 @@ package cf.naechelin.service.review;
 
 import cf.naechelin.dao.ReviewDAO;
 import cf.naechelin.exception.ReviewException;
+import cf.naechelin.vo.MissionVO;
+import cf.naechelin.vo.QueryVO;
 import cf.naechelin.vo.ReviewVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service("reviewUpdateService")
 public class ReviewUpdateServiceImpl implements ReviewUpdateService
@@ -16,7 +20,23 @@ public class ReviewUpdateServiceImpl implements ReviewUpdateService
     public void doService(int memberId, int storeId) throws ReviewException
     {
         ReviewVO review = new ReviewVO();
-        review.setLineId(reviewDAO.findLineId(memberId));
+        int lastLineNum=0;
+        ArrayList<Integer> lineList = reviewDAO.findLineId(memberId);
+        StringBuilder sb = new StringBuilder();
+        for(Integer num : lineList){
+            sb.append(num+",");
+            lastLineNum = num;
+        }
+        if(sb == null) { return ;}
+        sb.deleteCharAt(sb.length());
+        QueryVO query = new QueryVO();
+        query.setWord(sb.toString());
+        query.setIntWord(review.getStoreId());
+        MissionVO missionVO = reviewDAO.insertCheck(query);
+        if(missionVO.getMissionSuccessTime() == null){
+            throw new ReviewException("리뷰 수정 실패, 먼저 방문해 주세요.");
+        }
+        review.setLineId(lastLineNum);
         review.setStoreId(storeId);
         reviewDAO.reviewEdit(review);
     }
