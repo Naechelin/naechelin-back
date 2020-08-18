b3691bb0bb94270836710b95ec680b4f4e84f681
ssinsaram2 <ssinsaram2@hanmail.net>
2020-08-18 12:51:36 +0900

    Update QuestionAnswerDeleteServiceImpl.java

--- Naechelin/src/main/java/cf/naechelin/service/question/QuestionAnswerDeleteServiceImpl.java
+++ Naechelin/src/main/java/cf/naechelin/service/question/QuestionAnswerDeleteServiceImpl.java
@@ -5,7 +5,9 @@ import cf.naechelin.exception.QuestionException;
 import cf.naechelin.vo.AnswerVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service("questionAnswerDeleteService")
 public class QuestionAnswerDeleteServiceImpl implements QuestionAnswerDeleteService
 {
     @Autowired
