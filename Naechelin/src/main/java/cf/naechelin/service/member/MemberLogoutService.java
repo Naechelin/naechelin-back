6026e8ece665a3610e91de472af4e4f4276a4ba9
ssinsaram2 <ssinsaram2@hanmail.net>
2020-08-18 12:45:58 +0900

    Delete MemberLogoutService.java

--- Naechelin/src/main/java/cf/naechelin/service/member/MemberLogoutService.java
+++
@@ -1,9 +0,0 @@
-package cf.naechelin.service.member;
-
-import cf.naechelin.exception.MemberException;
-import cf.naechelin.vo.MemberVO;
-
-public interface MemberLogoutService extends MemberService
-{
-    void doService(MemberVO member) throws MemberException;
-}
